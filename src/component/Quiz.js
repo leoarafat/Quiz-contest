3b3fa5c0e8555156035abcd9fcbeb236884a0246
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-10-11 01:12:59 +0600

    show quiz to ui

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Quiz = ({QuizQuestion}) => {
-    // console.log(QuizQuestion)
-    const {question} = QuizQuestion
-    // console.log(question)
-    
-    
-    
-    return (
-        <div>
-            <h2>{question}</h2>
-        </div>
-    );
+const Quiz = ({ QuizQuestion }) => {
+  // console.log(QuizQuestion)
+  const { question, options } = QuizQuestion;
+  // console.log(question)
+
+  return (
+    <div className="border border-solid p-3 m-2">
+      <h2>{question}</h2>
+      <div className=" grid grid-cols-2 p-2 my-3">
+        {options.map((option) => (
+          <p className="my-2 ">
+            
+              <input type="radio" />
+              {option}
+            
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
